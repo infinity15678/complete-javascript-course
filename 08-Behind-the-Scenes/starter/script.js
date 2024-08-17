7cd3c5c3c571cc3962bd62aadf8e3f5e84d474f1
andy <andy.mandy@gmail.com>
2024-08-17 19:40:15 -0400

    added comments; Test from other machine

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -4,11 +4,14 @@ function calcAge(birthYear) {
     //function scope
     //exeuction context
     const age = 2037-birthYear;
+    //Another scope
+    // And another execution context
     function printAge() {
         const output = `You are ${firstName}, born in ${birthYear}`
         console.log(output);
-
+        //Since we are using use strict, blocks have scopes as well.
         if (birthYear >= 1981 && birthYear <= 1996){
+            //Var declarations do not apply since var is not part of Es6
             var millenial = true;
             const firstName = 'Steven';
             const str = `Oh and your're a millenial, ${firstName}`;
@@ -16,6 +19,7 @@ function calcAge(birthYear) {
 
            
         }
+        //Throws error;
         console.log(millenial);
     }
     printAge();
