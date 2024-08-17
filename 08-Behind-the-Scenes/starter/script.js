dbf176f4374f71733d971382750f563105f61946
infinity15678 <andy.mandy2001@icloud.com>
2024-08-17 19:32:04 -0400

    js behind the scenes part 1

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+//Global scope
+function calcAge(birthYear) {
+    //function scope
+    //exeuction context
+    const age = 2037-birthYear;
+    function printAge() {
+        const output = `You are ${firstName}, born in ${birthYear}`
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996){
+            var millenial = true;
+            const firstName = 'Steven';
+            const str = `Oh and your're a millenial, ${firstName}`;
+            console.log(str);
+
+           
+        }
+        console.log(millenial);
+    }
+    printAge();
+    return age;
+}
+
+const firstName = "andy";
+calcAge(1991);
